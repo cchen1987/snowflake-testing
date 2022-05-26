9a31b8c0a88c9a6240798077ae98b5b111a22219
cchen1987 <chen.11.4.87@gmail.com>
2022-05-27 00:53:12 +0200

    test

--- main.tf
+++ main.tf
@@ -2,12 +2,6 @@ module "snowflake" {
   source  = "app.terraform.io/chen1987/test/snowflake"
   version = "0.1.2"
 
-  providers = {
-    snowflake.sysadmin = snowflake.sysadmin
-    snowflake.securityadmin = snowflake.securityadmin
-    snowflake.accountadmin = snowflake.accountadmin 
-  }
-
   role_name = "test_role"
   database_name = "test_db"
 }
